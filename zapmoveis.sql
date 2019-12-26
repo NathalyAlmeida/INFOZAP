68a301fda98f637ff27c872670d5526a17f6f02a
NathalyAlmeida <59259149+NathalyAlmeida@users.noreply.github.com>
2019-12-26 20:28:41 -0300

    Add files via upload

--- zapmoveis.sql
+++ zapmoveis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 24/10/2019 às 20:18
--- Versão do servidor: 10.4.6-MariaDB
--- Versão do PHP: 7.3.8
+-- Tempo de geração: 27-Dez-2019 às 00:28
+-- Versão do servidor: 10.4.8-MariaDB
+-- versão do PHP: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,29 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `hotel`
+-- Banco de dados: `zapmoveis`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `user`
+-- Estrutura da tabela `moveis`
+--
+
+CREATE TABLE `moveis` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(40) NOT NULL,
+  `sobrenome` varchar(40) NOT NULL,
+  `data` date NOT NULL,
+  `movel` varchar(40) NOT NULL,
+  `preco` int(4) NOT NULL,
+  `descricao` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
 --
 
 CREATE TABLE `user` (
@@ -39,25 +55,37 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Despejando dados para a tabela `user`
+-- Extraindo dados da tabela `user`
 --
 
 INSERT INTO `user` (`id_user`, `name`, `lastname`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'Pedro Augusto', 'De Lima', 'pedroaugustoinfo@gmail.com', '1234', '2019-09-18 00:00:00', '2019-09-18 16:29:10');
+(1, 'Jezmael', 'Basilio', 'jezmael@info.com', '$2y$10$LDRX3vHgz9/wPPTPOVcFtOOGdnMj5S8MyMFSGgtg43COIthE5bSz2', '2019-09-18 00:00:00', '2019-09-18 16:29:10');
 
 --
--- Índices de tabelas apagadas
+-- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `user`
+-- Índices para tabela `moveis`
+--
+ALTER TABLE `moveis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT de tabelas apagadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `moveis`
+--
+ALTER TABLE `moveis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de tabela `user`
